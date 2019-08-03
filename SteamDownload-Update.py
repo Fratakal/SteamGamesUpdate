459b994f865bf1fc4b9602ab87cb117592e8c174
Fratakal <30315481+Fratakal@users.noreply.github.com>
2019-08-03 14:30:26 +0200

    Update SteamDownload-Update.py

--- SteamDownload-Update.py
+++ SteamDownload-Update.py
@@ -233,7 +233,7 @@ def update_mods():
             if not STEAM_USER or not STEAM_PASS:
                 steam_cmd_params  = " +login anonymous"
             else:
-                steam_cmd_params  = " +login {} {}".format(STEAM_USER, STEAM_PASS)
+                steam_cmd_params  = " +login '{}' '{}'".format(STEAM_USER, STEAM_PASS)
             steam_cmd_params += " +workshop_download_item {} {} validate".format(
                 WORKSHOP_GAME_ID,
                 mod_id
